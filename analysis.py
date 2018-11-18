af327500409183f0e87b13f83b43e1c284fca234
Jonas Powell <jonasori@gmail.com>
2018-11-17 23:47:18 -0500

    b

--- analysis.py
+++ analysis.py
@@ -17,8 +17,10 @@ from astropy.io import fits
 from matplotlib.pylab import *
 from matplotlib.ticker import *
 from matplotlib.pylab import figure
+import matplotlib.gridspec as gridspec
 from matplotlib.patches import Ellipse as ellipse
 from astropy.visualization import astropy_mpl_style
+
 from constants import lines, get_data_path, obs_stuff, offsets, get_data_path, mol
 
 plt.style.use(astropy_mpl_style)
@@ -60,10 +62,8 @@ def depickleLogFile(fname):
             disk_B[i]['xvals_queried'] = list(set(ps_B))
             disk_B[i]['name'] = p
 
-    both_disks = [
-     disk_A, disk_B]
-    return (
-     both_disks, X2s)
+    both_disks = [disk_A, disk_B]
+    return (both_disks, X2s)
 
 
 def plot_gridSearch_log(fname, show=False):
@@ -85,7 +85,8 @@ def plot_gridSearch_log(fname, show=False):
     f, axarr = plt.subplots(len(disk_A) + 1, 2, figsize=[8, 8])
     axarr[(0, 0)].axis('off')
     axarr[(0, 1)].axis('off')
-    axarr[(0, 0)].text(0.2, -0.2, 'Summary of\n' + run_date + ' Run', fontsize=16, fontweight='bold')
+    axarr[(0, 0)].text(0.2, -0.2, 'Summary of\n' + run_date + ' Run',
+                       fontsize=16, fontweight='bold')
     chi_str = 'Min. Raw Chi2: ' + str(min(raw_x2)) + '\nMin. Reduced Chi2: ' + str(min(red_x2))
     axarr[(0, 1)].text(0, 0, chi_str, fontsize=10)
     for d in [0, 1]:
@@ -98,8 +99,11 @@ def plot_gridSearch_log(fname, show=False):
             axarr[(i, d)].plot(xs, [0] * 2, '-k')
             for bf in p['best_fits']:
                 a = 1 / (2 * len(p['best_fits']))
-                axarr[(i, d)].plot(bf, 0, marker='o', markersize=10, color='black', alpha=a)
-                axarr[(i, d)].plot(bf, 0, marker='o', markersize=9, color=colors[d], markerfacecolor='none', markeredgewidth=3)
+                axarr[(i, d)].plot(bf, 0, marker='o', markersize=10,
+                                   color='black', alpha=a)
+                axarr[(i, d)].plot(bf, 0, marker='o', markersize=9,
+                                   color=colors[d], markerfacecolor='none',
+                                   markeredgewidth=3)
 
     plt.tight_layout()
     plt.savefig(resultsPath + run_date + '_results.png', dpi=200)
@@ -137,13 +141,15 @@ def plot_step_duration(dataPath, ns=[10, 20, 50], show=False):
     for i in range(len(ns)):
         n = ns[i]
         avg_ys = get_rolling_avg(xs, ys, n)
-        plt.plot(xs[n / 2:-n / 2], avg_ys, linestyle='-', color=colors[i], linewidth=0.1 * n, label=str(n) + '-step smoothing')
+        plt.plot(xs[n / 2:-n / 2], avg_ys, linestyle='-', color=colors[i],
+                 linewidth=0.1 * n, label=str(n) + '-step smoothing')
 
     run_date = dataPath.split('/')[-1]
     plt.legend()
     plt.xlabel('Step', fontweight='bold')
     plt.ylabel('Time (minutes)', fontweight='bold')
-    plt.title('Time per Step for Grid Search Run on ' + run_date, fontweight='bold', fontsize=14)
+    plt.title('Time per Step for Grid Search Run on ' + run_date,
+              fontweight='bold', fontsize=14)
     plt.savefig(resultsPath + run_date + '_durations.png', dpi=200)
     if show is True:
         plt.show()
@@ -180,7 +186,9 @@ def full_analysis_plot(pickleLog, timeLog):
     ax_top.axis('off')
     ax_top.text(0.2, 0.2, run_date + ' Run Summary', fontsize=20, fontweight='bold')
     fig.add_subplot(ax_top)
-    inner = gridspec.GridSpecFromSubplotSpec(len(both_disks[0]), 2, subplot_spec=outer[1], wspace=0.1, hspace=1)
+    inner = gridspec.GridSpecFromSubplotSpec(len(both_disks[0]), 2,
+                                             subplot_spec=outer[1],
+                                             wspace=0.1, hspace=1)
     for d in range(2):
         params = both_disks[d]
         for i, p in enumerate(params):
@@ -191,7 +199,8 @@ def full_analysis_plot(pickleLog, timeLog):
             ax.xaxis.set_ticks(p['xvals_queried'])
             ax.plot(xs, [0] * len(xs), '-k')
             for bf in p['best_fits']:
-                ax.plot(bf, 0, marker='o', markersize=9, color=colors[d], markerfacecolor='none', markeredgewidth=2)
+                ax.plot(bf, 0, marker='o', markersize=9, color=colors[d],
+                        markerfacecolor='none', markeredgewidth=2)
 
             fig.add_subplot(ax)
 
@@ -202,17 +211,22 @@ def full_analysis_plot(pickleLog, timeLog):
     for i in range(len(ns)):
         n = ns[i]
         avg_ys = get_rolling_avg(steps, times, n)
-        ax_bottom.plot(steps[n / 2:-n / 2], avg_ys, linestyle='-', color=colors[i], linewidth=0.1 * n, label=str(n) + '-step smoothing')
+        ax_bottom.plot(steps[n / 2:-n / 2], avg_ys, linestyle='-',
+                       color=colors[i], linewidth=0.1 * n,
+                       label=str(n) + '-step smoothing')
 
     ax_bottom.legend()
     ax_bottom.set_xlabel('Step', fontweight='bold')
     ax_bottom.set_ylabel('Time (minutes)', fontweight='bold')
-    ax_bottom.set_title('Time per Step for Grid Search Run on ' + run_date, fontweight='bold', fontsize=14)
+    ax_bottom.set_title('Time per Step for Grid Search Run on ' + run_date,
+                        fontweight='bold', fontsize=14)
     fig.add_subplot(ax_bottom)
     fig.show()
 
 
-def plot_fits(image_path, mol=mol, scale_cbar_to_mol=False, crop_arcsec=2, cmap='magma', save=True, show=True, use_cut_baselines=True, best_fit=False):
+def plot_fits(image_path, mol=mol, scale_cbar_to_mol=False, crop_arcsec=2,
+              cmap='magma', save=True, show=True,
+              use_cut_baselines=True, best_fit=False):
     """
     Plot some fits image data.
 
@@ -333,119 +347,12 @@ def plot_fits(image_path, mol=mol, scale_cbar_to_mol=False, crop_arcsec=2, cmap=
     plt.gca()
 
 
-def plot_model_and_data(image_path, mol=mol, scale_cbar_to_mol=False,
-                        crop_arcsec=2, cmap='magma',
-                        save=True, show=True, use_cut_baselines=True):
+
+def plot_model_and_data():
+    """Plot a triptych of data, model, and residuals.
+
+    It would be nice to have an option to also plot the grid search results.
     """
-    Make a two-panel plot comparing data and model.
-
-    NOT CURRENTLY IMPLEMENTED. NEEDS FULL REWRITE.
-
-    The cropping currently assumes a square image. That could be easily
-    fixed by just adding y_center, y_min, and y_max and putting them in the
-    imshow() call.
-    Args:
-        image_path (str): full path, including filetype, to image.
-        crop_arcsec (float): How many arcseconds from 0 should the axis limits be set?
-        nchans_to_cut (int): cut n/2 chans off the front and end
-        cmap (str): colormap to use. Magma, copper, afmhot, CMRmap, CMRmap(_r) are nice
-
-    Known Bugs:
-        - Some values of n_chans_to_cut give a weird error. I don't really wanna
-            figure that out right now
-
-    To Do:
-        - Maybe do horizontal layout for better screenshots for Evernote.
-    """
-    real_data = fits.getdata(image_path, ext=0).squeeze()
-    header = fits.getheader(image_path, ext=0)
-    if scale_cbar_to_mol is True:
-        dataPath = get_data_path(mol, use_cut_baselines=True)
-        real_data = fits.getdata(dataPath + '.fits', ext=0).squeeze()
-        vmin = np.nanmin(real_data)
-        vmax = np.nanmax(real_data)
-    else:
-        vmin = np.nanmin(real_data)
-        vmax = np.nanmax(real_data)
-    offsets_dA, offsets_dB = offsets[0], offsets[1]
-    add_beam = True if 'bmaj' in header else False
-    if add_beam is True:
-        bmin = header['bmin'] * 3600.0
-        bmaj = header['bmaj'] * 3600.0
-        bpa = header['bpa']
-    x_center = int(np.floor(real_data.shape[1] / 2))
-    if crop_arcsec == 0:
-        crop_arcsec = 11.52
-    crop_pix = int(crop_arcsec / 0.045)
-    xmin, xmax = x_center - crop_pix, x_center + crop_pix
-    chanstep_vel = header['CDELT3'] * 0.001
-    chan0_vel = header['CRVAL3'] * 0.001 - header['CRPIX3'] * chanstep_vel
-    nchans_to_cut = 12
-    chan_offset = 2 * int(nchans_to_cut / 3)
-    nchans = real_data.shape[0] - nchans_to_cut
-    n_rows = int(np.floor(np.sqrt(nchans)))
-    n_cols = int(np.ceil(np.sqrt(nchans)))
-    chan_offset = 4
-    fig = figure(figsize=[n_rows, n_cols])
-    for i in range(nchans):
-        chan = i + int(np.floor(nchans_to_cut / 2))
-        velocity = str(round(chan0_vel + chan * chanstep_vel, 2))
-        ax = fig.add_subplot(n_cols, n_rows, i + 1)
-        ax.grid(False)
-        ax.set_xticklabels([])
-        ax.set_yticklabels([])
-        ax.text(0, -0.8 * crop_arcsec, velocity + ' km/s', fontsize=6, color='w', horizontalalignment='center', verticalalignment='center')
-        if i == n_rows * (n_cols - 1) and add_beam is True:
-            el = ellipse(xy=[0.8 * crop_arcsec, 0.8 * crop_arcsec], width=bmin, height=bmaj, angle=-bpa, fc='k', ec='w', fill=False, hatch='////////')
-            ax.add_artist(el)
-        cmaps = imshow(real_data[i + chan_offset][xmin:xmax, xmin:xmax], cmap=cmap, vmin=vmin, vmax=vmax, extent=(
-         crop_arcsec, -crop_arcsec,
-         crop_arcsec, -crop_arcsec))
-        ax.plot(offsets_dA[0], offsets_dA[1], '+g')
-        ax.plot(offsets_dB[0], offsets_dB[1], '+g')
-
-    inset_cbar = True
-    if inset_cbar is True:
-        plt.tight_layout()
-        fig.subplots_adjust(wspace=0.1, hspace=0.1)
-        cax = plt.axes([0.55, 0.08, 0.38, 0.07])
-        cbar = colorbar(cmaps, cax=cax, orientation='horizontal')
-        cbar.set_label('Jy/beam', labelpad=-12, fontsize=12, weight='bold')
-        cbar.set_ticks([vmin, vmax])
-    else:
-        plt.tight_layout()
-        fig.subplots_adjust(wspace=0.1, hspace=0.1, top=0.9)
-        cax = plt.axes([0.1, 0.95, 0.81, 0.025])
-        cbar = colorbar(cmaps, cax=cax, orientation='horizontal')
-        cbar.set_label('Jy/beam', labelpad=-12, fontsize=12, weight='bold')
-        cbar.set_ticks([vmin, vmax])
-    if save is True:
-        suffix = ''
-        if 'data' in image_path:
-            resultsPath = 'data/' + mol + '/images/'
-            if '-short' in image_path:
-                suffix = '-' + image_path.split('-')[1].split('.')[0]
-        else:
-            if 'mcmc' in image_path:
-                resultsPath = 'mcmc_results/'
-            else:
-                if 'gridsearch' in image_path:
-                    resultsPath = 'gridsearch_results/'
-                else:
-                    return 'Failed to make image; specify save location.'
-        run_name = image_path.split('/')[-2]
-        outpath = resultsPath + run_name + suffix + '_image.png'
-        plt.savefig(outpath, dpi=200)
-        print 'Image saved to ' + outpath
-    if show is True:
-        plt.show(block=False)
-    plt.gca()
-
-
-import matplotlib.pyplot as plt, matplotlib.gridspec as gridspec
-
-def plot_model_and_data_gridspec():
-    """Should add residuals as well while I'm at it."""
     plt.close()
 
     # Read in the data
@@ -543,7 +450,7 @@ def plot_model_and_data_gridspec():
     fig.show()
 
 
-def plot_param_degeneracies(dataPath, param1, param2, DI=0):
+def plot_param_degeneracies(dataPath='gridsearch_runs/nov8_hcn/nov8_hcn', DI=0):
     """Plot Chi2 as a function of two params.
 
     I think this works now.
@@ -551,18 +458,31 @@ def plot_param_degeneracies(dataPath, param1, param2, DI=0):
     df_raw = pickle.load(open(('{}_step-log.pickle').format(dataPath), 'rb'))
     df_full = df_raw.loc['A', :] if DI == 0 else df_raw.loc['B', :]
     l = list(df_full.columns)
+
+    for i, p in enumerate(l):
+        len_p = len(list(set(df_full[p])))
+        if len_p > 1:
+            if p != 'Reduced Chi2' and p != 'Raw Chi2':
+                print str([i]), p, ['Length: ' + len_p]
+
+    p1_idx = int(raw_input('Select the index of the first parameter.\n'))
+    p2_idx = int(raw_input('Select the index of the second parameter.\n'))
+    param1, param2 = l[p1_idx], l[p2_idx]
+
+    # Clear out the parameters that we're not interested in
     l.remove(param1)
     l.remove(param2)
-    l.remove('Reduced Chi2')
+    l.remove('Raw Chi2')
     df = df_full
     for p in l:
         p_vals = list(set(df[p]))
-        if len(p_vals) == 1 or p == 'Raw Chi2':
+        if len(p_vals) == 1 or p == 'Reduced Chi2':
             df = df.drop(p, axis=1)
         else:
             df = df.drop(df[df[p] != df[p][0]].index)
-
     df = df.reset_index(drop=True)
+
+    # Make sure we're looking at an iterated parameter.
     len_p1, len_p2 = len(list(set(df[param1]))), len(list(set(df[param2])))
     print len_p1, len_p2
     if len_p1 < 2 or len_p2 < 2:
@@ -573,22 +493,27 @@ def plot_param_degeneracies(dataPath, param1, param2, DI=0):
     else:
         p_i, p_j = param1, param2
         len_p_i, len_p_j = len_p1, len_p2
+
+    # Populate our chi-squared grid
     mat = np.zeros((len_p_i, len_p_j))
     for i in range(len_p_i):
         for j in range(len_p_j):
-            mat[(i, j)] = df['Reduced Chi2'][i * len_p_j + j]
-            print p_i, df[p_i][i * len_p_j + j]
-            print p_j, df[p_j][i * len_p_j + j]
+            this_chi = df['Raw Chi2'][i * len_p_j + j]
+            mat[(i, j)] = this_chi
+            print this_chi
+            print p_i, df[p_i][i * len_p_j + j], '; ', p_j, df[p_j][i * len_p_j + j]
+            print
 
     plt.matshow(mat, cmap='jet')
     plt.xlabel(df[p_j].name)
     plt.ylabel(df[p_i].name)
+    plt.xticks(range(len_p2), sorted(list(set(df[param2]))))
+    plt.yticks(range(len_p1), sorted(list(set(df[param1]))))
     plt.title('Chi2 Map Over Params')
     plt.grid(False)
     plt.colorbar()
     plt.show(block=False)
     return mat
-
 
 
 
