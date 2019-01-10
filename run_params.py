2b4b0cc5dea6dbcf052859d528d5cdca911cdda0
Jonas Powell <jonasori@gmail.com>
2019-01-10 14:19:04 -0500

    Updated

--- run_params.py
+++ run_params.py
@@ -42,7 +42,7 @@ def make_diskA_params(mol, run_length='long'):
         t_atms      = np.arange(10, 300, 50)
         t_qq        = -1 * np.array([0])
         r_out       = np.arange(50, 500, 75)
-        x_mol       = -1 * np.arange(2, 9)
+        x_mol       = -1 * np.arange(2, 10)
 
     else:
         return "Please choose 'short', 'mid', or 'long'"
@@ -107,7 +107,7 @@ def make_diskB_params(mol, run_length='long'):
         t_atms      = np.arange(10, 300, 50)
         t_qq        = -1 * np.array([-0.5, 0, 0.5])
         r_out       = np.arange(50, 400, 50)
-        x_mol       = -1 * np.arange(2, 9)
+        x_mol       = -1 * np.arange(2, 10)
 
     else:
         return "Please choose 'short', 'mid', or 'long'"
