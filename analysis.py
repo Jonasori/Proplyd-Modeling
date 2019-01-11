2390dba82ccdffa2d7b54caf47a8140a4e4a6cde
Jonas Powell <jonasori@gmail.com>
2019-01-11 12:41:24 -0500

    Updated

--- analysis.py
+++ analysis.py
@@ -532,7 +532,7 @@ def plot_model_and_data(modelPath, mol='cs', save=False, cmap='magma'):
                            crop_arcsec, -crop_arcsec))
 
     fig.subplots_adjust(wspace=0.1, hspace=0.1)
-    cax = plt.axes([0.2, 0.1, 0.6, 0.05])
+    cax = plt.axes([0.2, 0.05, 0.6, 0.05])
     cbar = colorbar(cmaps, cax=cax, orientation='horizontal')
     cbar.set_label('Jy/beam', labelpad=-12, fontsize=12, weight='bold')
     cbar.set_ticks([vmin, vmax])
