af57796ed38030e44ebefb663292ea6b36292ab6
Jonas Powell <jonasori@gmail.com>
2019-01-14 18:26:03 -0500

    Updated

--- analysis.py
+++ analysis.py
@@ -9,6 +9,7 @@ import pickle
 import argparse
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import subprocess as sp
 import matplotlib.pyplot as plt
 # import matplotlib.pyplot.RcParams
