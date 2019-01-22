70ab292c9bbe7d0ddf461aba683407b130d277ad
Jonas Powell <jonasori@gmail.com>
2019-01-22 11:47:01 -0500

    Updated

--- mcmc.py
+++ mcmc.py
@@ -547,7 +547,7 @@ def run_emcee(run_path, run_name, mol, nsteps, nwalkers, lnprob):
                               lnpostfn returns blobs too.
             """
     lnprobs = []
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
 
     print "About to loop over run"
     for i, result in enumerate(run):
